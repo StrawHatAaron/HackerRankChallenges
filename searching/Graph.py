87d5b9dcf17a728a274754d56fb0f919e9206dd9
aaronmiller <aaronmiller822028@gmail.com>
2017-11-29 03:35:26 -0800

    Couldnt fall asleep so went back to it for a after a break(:

--- searching/Graph.py
+++ searching/Graph.py
@@ -1,3 +1,14 @@
+#Aaron Miller 11/28/2017
+
+#q=given queries            first
+
+#n=number of cities         second
+#m=number of roads
+#c_lib=cost of library
+#c_road=cost of road
+
+#from_city,to_city=bisectional roads      third
+
 import sys
 class Graph():
     def __init__(self, m):
@@ -6,16 +17,55 @@ class Graph():
 
     #find shortest path
     def dijkstra(self):
-        min_path = [100000] * self.m
-        #min_path = [n] no starting node so this wont work
-        smallSet = [False] * self.m
-        for x in xrange(self.m):
-            print("NOT DONE HERE YET")
-        return graph
+        min_path = [100000] * self.roads
+        smallest_set = [False] * self.roads
+        number_of_nodes=self.count_nodes()
+        connected_nodes=self.connect_nodes()#what nodes tie to eachother
+        answer = []
 
-    #search by depth and return each nodes connections to other nodes
-    #def dfs_connect(self):
-    #   return self
+        for x in range(len(self.graph)):
+            for y in range (len(self.graph)):
+                print("LEFT OFF HERE")
+                #if self.graph[x][y]>0 and 
+        return answer
+
+    # A utility function to find the vertex with 
+    # minimum distance value, from the set of vertices 
+    # not yet included in shortest path tree
+    def minDistance(self, dist, sptSet):
+ 
+        # Initilaize minimum distance for next node
+        min = sys.maxint
+ 
+        # Search not nearest vertex not in the 
+        # shortest path tree
+        for v in range(self.V):
+            if dist[v] < min and sptSet[v] == False:
+                min = dist[v]
+                min_index = v
+ 
+        return min_index
+
+    #find the number of node connections per node at index
+    def count_nodes(self):
+        number_of_nodes = [[0]for path in range(len(self.graph))]
+        for x in range(len(self.graph)):
+            number_of_nodes[x]=0
+        for x in range(len(self.graph)):
+            for y in range(len(self.graph)):
+                if self.graph[x][y]==1:
+                    number_of_nodes[x]=number_of_nodes[x]+1
+        return number_of_nodes
+
+    #find the values each node connects directly to
+    def connect_nodes(self):
+        connected_nodes = [[0 for column in range(len(self.graph))] for row in range(len(self.graph))]
+        for x in range(len(self.graph)):
+            for y in range(len(self.graph)):
+                if self.graph[x][y]==1:
+                    #y->to  x should be the index
+                    connected_nodes[x][y]= y+1
+        return connected_nodes
 
 if __name__ == "__main__":
     total_cost=0
@@ -27,19 +77,9 @@ if __name__ == "__main__":
         for roads in range(m):
             from_city, to_city = input().strip().split()
             from_city, to_city = [int(from_city)-1, int(to_city)-1]
+            #fill up the graph with the broken roads
             g.graph[from_city][to_city]=1
             g.graph[to_city][from_city]=1
-            
-                
-
-
-#Aaron Miller 11/28/2017
-
-#q=given queries            first
-
-#n=number of cities         second
-#m=number of roads
-#c_lib=cost of library
-#c_road=cost of road
-
-#from_city,to_city=bisectional roads      third
+            print()
+            print(g.graph)
+            print(g.dijkstra())
